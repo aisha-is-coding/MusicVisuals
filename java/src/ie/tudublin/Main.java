4009b989fad45907cb1310ebd9276812fe89c3c3
Aisha Adebimpe <aishaadebimpe@gmail.com>
2023-05-03 00:43:04 +0100

    starting over

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -7,24 +7,24 @@ package ie.tudublin;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		processing.core.PApplet.main(new String[] { MyVisual.class.getName() });
-		}
+	// public static void main(String[] args) {
+	// 	processing.core.PApplet.main(new String[] { MyVisual.class.getName() });
+	// 	}
 	
 	
 	
 	
 	
-	// public void startUI()
-	// {
-	//   	String[] a = {"MAIN"};
-	//   	processing.core.PApplet.runSketch( a, new MyVisual());
-	//  }
-	//   public static void main(String[] args)
-	//   {
-	// 	Main main = new Main();
-	// 	main.startUI();
-	//  } 
+	public void startUI()
+	{
+	  	String[] a = {"MAIN"};
+	  	processing.core.PApplet.runSketch( a, new stars());
+	 }
+	  public static void main(String[] args)
+	  {
+		Main main = new Main();
+		main.startUI();
+	} 
 	 
 
 		
