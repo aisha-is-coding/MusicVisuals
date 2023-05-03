d226e72cf988e8a844ef1471d13bf3ccf78f50b0
Aisha Adebimpe <aishaadebimpe@gmail.com>
2023-05-03 01:01:06 +0100

    still can't find a solution

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -18,7 +18,7 @@ public class Main {
 	public void startUI()
 	{
 	  	String[] a = {"MAIN"};
-	  	processing.core.PApplet.runSketch( a, new stars());
+	  	processing.core.PApplet.runSketch( a, new Basic());
 	 }
 	  public static void main(String[] args)
 	  {
